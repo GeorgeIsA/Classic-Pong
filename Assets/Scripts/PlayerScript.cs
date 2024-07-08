45c4ee85aa92489a0c3281afcb1f618a2595343d
georgeisonearth <89208162+GeorgeIsA@users.noreply.github.com>
2024-07-09 02:28:58 +0300

    player collides with walls and there is a ball

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class PlayerScript : MonoBehaviour
-{ 
-    public int speed =  10;
-    void Start()
+{
+    public int speed = 10;
+    bool moving = false;
+    Rigidbody2D rb2d;
+    void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector2.up * Time.deltaTime * speed);
-        if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector2.down * Time.deltaTime * speed);
-
-
+            rb2d.velocity = Vector2.up * speed;
+        else if (Input.GetKey(KeyCode.S))
+            rb2d.velocity = Vector2.down * speed;
+        else
+            rb2d.velocity = new Vector2(0, 0);
+   
     }
 }
