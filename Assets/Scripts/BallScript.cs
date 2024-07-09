2e3fffc4240515318ffa124c18d67944bfd0c43b
georgeisonearth <89208162+GeorgeIsA@users.noreply.github.com>
2024-07-09 18:26:18 +0300

    Player bounces the ball at a different y position based on the edge of the paddle

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -4,24 +4,45 @@ public class BallScript : MonoBehaviour
 {
     Rigidbody2D rb2d;
     float playerSpeed;
+    public float scaleY = 10;
+
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
+
     private void FixedUpdate()
     {
         playerSpeed = GameObject.Find("player").GetComponent<PlayerScript>().globalSpeed;
-    
     }
+
     private void Update()
     {
         Debug.Log(rb2d.velocity.x);
     }
-    void OnCollisionEnter2D()
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (rb2d.velocity.x > 0)
-            rb2d.velocity = new Vector2(1, 0) * playerSpeed;
-        else if (rb2d.velocity.x < 0)
-            rb2d.velocity = new Vector2 (-1, 0) * playerSpeed;
+        float playerY = GetY(other);
+        float ballY = gameObject.transform.position.y;
+        if (other.collider.CompareTag("Player"))
+        {
+            if (rb2d.velocity.x > 0)
+                rb2d.velocity = new Vector2(1, (ballY - playerY) * scaleY * 0.1f) * playerSpeed;
+            else if (rb2d.velocity.x < 0)
+                rb2d.velocity = new Vector2(-1, (ballY - playerY) * scaleY * 0.1f) * playerSpeed;
+        }
+        else if (other.collider.CompareTag("edge"))
+        {
+            if (rb2d.velocity.x > 0)
+                rb2d.velocity = new Vector2(1, 0) * playerSpeed;
+            else if (rb2d.velocity.x < 0)
+                rb2d.velocity = new Vector2(-1, 0) * playerSpeed;
+        }
+    }
+
+    private float GetY(Collision2D other)
+    {
+        return other.gameObject.transform.position.y;
     }
 }
